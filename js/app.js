2f7dc35728b54e0c9ebf2d17bad9eb9d5f2b7494
Carlos Guedes <cguedes@gmail.com>
2013-04-20 00:20:08 +0100

    Initial emberjs version

--- js/app.js
+++ js/app.js
@@ -3,22 +3,6 @@ App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
-App.Post = [{
-  id: 1,
-  title: "Rais is Omask",
-  author: "d2h",
-  publishedAt: new Date("12-27-2012"),
-  intro: "There are lost of a la carte software developers in the world.",
-  extended: "I want this for my ORM, I want for my template language to bla bla bla"
-}, {
-  id: 2,
-  title: "Gread day",
-  author: "d2h",
-  publishedAt: new Date("12-24-2012"),
-  intro: "Today is a summy and beautifull day.",
-  extended: "The days with sun are **amazing** days. Those `with clouds are` not that cool!"
-}];
-
 App.Router.reopen({
   //location: 'history'
 });
@@ -26,21 +10,7 @@ App.Router.reopen({
 
 App.Router.map(function() {
   this.resource("about");
-  this.resource("posts", function(){
-    this.route("post", { path: "/:post_id" });
-  });
-});
-
-/*
-App.PostsPostRoute = Ember.Route.extend({
-  model: function(params) {
-    return posts.filter(function(post) { return post.id == params.post_id; })[0];
-  }
-});
-*/
-
-App.PostsController = Ember.ArrayController.extend({
-  posts: App.Post
+  this.resource("contact");
 });
 
 
